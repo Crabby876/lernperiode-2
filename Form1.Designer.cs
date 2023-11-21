8407944d9bcf1d775a4b9bee12685dc2f9f0543b
Crabby876 <142886360+Crabby876@users.noreply.github.com>
2023-11-21 11:52:38 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Schere__Stein_Papier
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BScissour = new Button();
+            BStone = new Button();
+            BPaper = new Button();
+            labelVS = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            RESET = new Button();
+            B3pt = new Button();
+            B1pt = new Button();
+            BMenu = new Button();
+            SuspendLayout();
+            // 
+            // BScissour
+            // 
+            BScissour.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            BScissour.Location = new Point(726, 389);
+            BScissour.Name = "BScissour";
+            BScissour.Size = new Size(171, 135);
+            BScissour.TabIndex = 0;
+            BScissour.Text = "✂️";
+            BScissour.UseVisualStyleBackColor = true;
+            BScissour.Visible = false;
+            BScissour.Click += BScissour_Click;
+            // 
+            // BStone
+            // 
+            BStone.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            BStone.Location = new Point(411, 389);
+            BStone.Name = "BStone";
+            BStone.Size = new Size(171, 135);
+            BStone.TabIndex = 1;
+            BStone.Text = "🗿";
+            BStone.UseVisualStyleBackColor = true;
+            BStone.Visible = false;
+            BStone.Click += BStone_Click;
+            // 
+            // BPaper
+            // 
+            BPaper.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            BPaper.Location = new Point(58, 389);
+            BPaper.Name = "BPaper";
+            BPaper.Size = new Size(171, 135);
+            BPaper.TabIndex = 2;
+            BPaper.Text = "📃";
+            BPaper.UseVisualStyleBackColor = true;
+            BPaper.Visible = false;
+            BPaper.Click += BPaper_Click;
+            // 
+            // labelVS
+            // 
+            labelVS.AutoSize = true;
+            labelVS.Font = new Font("Showcard Gothic", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            labelVS.Location = new Point(366, 183);
+            labelVS.Name = "labelVS";
+            labelVS.Size = new Size(241, 177);
+            labelVS.TabIndex = 3;
+            labelVS.Text = "VS";
+            labelVS.Visible = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(369, -8);
+            label1.Name = "label1";
+            label1.Size = new Size(238, 177);
+            label1.TabIndex = 4;
+            label1.Text = "📃";
+            label1.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(369, 353);
+            label2.Name = "label2";
+            label2.Size = new Size(238, 177);
+            label2.TabIndex = 5;
+            label2.Text = "📃";
+            label2.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Black", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Lime;
+            label3.Location = new Point(272, 22);
+            label3.Name = "label3";
+            label3.Size = new Size(401, 128);
+            label3.TabIndex = 6;
+            label3.Text = "Winner";
+            label3.Visible = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Black", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.Lime;
+            label4.Location = new Point(272, 382);
+            label4.Name = "label4";
+            label4.Size = new Size(401, 128);
+            label4.TabIndex = 7;
+            label4.Text = "Winner";
+            label4.Visible = false;
+            // 
+            // RESET
+            // 
+            RESET.BackColor = SystemColors.ButtonFace;
+            RESET.Font = new Font("Showcard Gothic", 25F, FontStyle.Bold, GraphicsUnit.Point);
+            RESET.ForeColor = Color.Black;
+            RESET.Location = new Point(902, 12);
+            RESET.Name = "RESET";
+            RESET.Size = new Size(58, 67);
+            RESET.TabIndex = 9;
+            RESET.Text = "🔄";
+            RESET.UseVisualStyleBackColor = false;
+            RESET.Visible = false;
+            RESET.Click += RESET_Click;
+            // 
+            // B3pt
+            // 
+            B3pt.Font = new Font("Showcard Gothic", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            B3pt.Location = new Point(508, 199);
+            B3pt.Name = "B3pt";
+            B3pt.Size = new Size(414, 131);
+            B3pt.TabIndex = 10;
+            B3pt.Text = "3pt.";
+            B3pt.UseVisualStyleBackColor = true;
+            B3pt.Click += B3pt_Click;
+            // 
+            // B1pt
+            // 
+            B1pt.Font = new Font("Showcard Gothic", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            B1pt.Location = new Point(43, 199);
+            B1pt.Name = "B1pt";
+            B1pt.Size = new Size(414, 131);
+            B1pt.TabIndex = 11;
+            B1pt.Text = "1pt.";
+            B1pt.UseVisualStyleBackColor = true;
+            B1pt.Click += B1pt_Click;
+            // 
+            // BMenu
+            // 
+            BMenu.BackColor = Color.WhiteSmoke;
+            BMenu.Font = new Font("Cooper Black", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            BMenu.Location = new Point(12, 12);
+            BMenu.Name = "BMenu";
+            BMenu.Size = new Size(122, 54);
+            BMenu.TabIndex = 12;
+            BMenu.Text = "MENU";
+            BMenu.UseVisualStyleBackColor = false;
+            BMenu.Visible = false;
+            BMenu.Click += BMenu_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(972, 587);
+            Controls.Add(BMenu);
+            Controls.Add(B1pt);
+            Controls.Add(B3pt);
+            Controls.Add(RESET);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(labelVS);
+            Controls.Add(BPaper);
+            Controls.Add(BStone);
+            Controls.Add(BScissour);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BScissour;
+        private Button BStone;
+        private Button BPaper;
+        private Label labelVS;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button RESET;
+        private Button B3pt;
+        private Button B1pt;
+        private Button BMenu;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,437 @@
+﻿namespace Schere__Stein_Papier
+
+{
+    using System;
+    using System.Threading.Tasks;
+    public partial class Form1 : Form
+    {
+        string NutzerEntscheidung;
+        string SchereSteinPapier;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool MehrPunkte = false;
+        private int runde = 0;
+        private int winBot = 0;
+        private int winPlayer = 0;
+        private void MenuAnsicht()
+        {
+            RESET.Visible = false;
+            BMenu.Visible = false;
+            B1pt.Visible = true;
+            B3pt.Visible = true;
+            BStone.Visible = false;
+            BScissour.Visible = false;
+            BPaper.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            labelVS.Visible = false;
+            label3.Text = "";
+            label4.Text = "";
+            NutzerEntscheidung = "";
+            SchereSteinPapier = "";
+        }
+
+        private void SpielAnsicht()
+        {
+            RESET.Visible = false;
+            BMenu.Visible = true;
+            B1pt.Visible = false;
+            B3pt.Visible = false;
+            BStone.Visible = true;
+            BScissour.Visible = true;
+            BPaper.Visible = true;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            labelVS.Visible = false;
+            label3.Text = "";
+            label4.Text = "";
+            NutzerEntscheidung = "";
+            SchereSteinPapier = "";
+        }
+        private void AuswahlGegner()
+        {
+            Random rzahl = new Random();
+            int Entscheidung = rzahl.Next(1, 4);
+
+            switch (Entscheidung)
+            {
+                case 1:
+                    SchereSteinPapier = "Schere";
+                    label1.Text = "✂️";
+                    break;
+                case 2:
+                    SchereSteinPapier = "Stein";
+                    label1.Text = "🗿";
+                    break;
+                case 3:
+                    SchereSteinPapier = "Papier";
+                    label1.Text = "📃";
+                    break;
+            }
+        }
+
+        private void BScissour_Click(object sender, EventArgs e)
+        {
+            NutzerEntscheidung = "Schere";
+            label2.Text = "✂️";
+            AuswahlGegner();
+            BStone.Visible = false;
+            BScissour.Visible = false;
+            BPaper.Visible = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            labelVS.Visible = true;
+
+            if (MehrPunkte)
+            {
+                switch (SchereSteinPapier)
+                {
+                    case "Schere":
+                        label3.Text = "  DRAW";
+                        label3.ForeColor = Color.Blue;
+                        label4.Text = "  DRAW";
+                        label4.ForeColor = Color.Blue;
+
+                        break;
+                    case "Stein":
+                        label3.Text = "WINNER";
+                        label3.ForeColor = Color.Green;
+                        label4.Text = "  LOSER";
+                        label4.ForeColor = Color.Red;
+                        winBot = winBot + 1;
+                        break;
+                    case "Papier":
+                        label3.Text = "  LOSER";
+                        label3.ForeColor = Color.Red;
+                        label4.Text = "WINNER";
+                        label4.ForeColor = Color.Green;
+                        winPlayer = winPlayer + 1;
+                        break;
+                }
+                Task.Delay(2000).Wait();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+                Task.Delay(2000).Wait();
+                if (winBot + winPlayer == 3)
+                {
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    label1.Text = "PL.1";
+                    label2.Text = "PL.2";
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    Task.Delay(2000).Wait();
+                    if (winBot < winPlayer)
+                    {
+                        label3.Text = "WINNER";
+                        label4.Text = "  LOSER";
+                    }
+                    else if (winBot > winPlayer)
+                    {
+                        label3.Text = "  LOSER";
+                        label4.Text = "WINNER";
+                    }
+
+                    label1.Visible = false;
+                    label2.Visible = false;
+                    labelVS.Visible = false;
+                    Task.Delay(2000).Wait();
+                    label3.Visible = true;
+                    label4.Visible = true;
+                }
+                else
+                {
+                    SpielAnsicht();
+                }
+            }
+            else
+            {
+                switch (SchereSteinPapier)
+                {
+                    case "Schere":
+                        label3.Text = "  DRAW";
+                        label3.ForeColor = Color.Blue;
+                        label4.Text = "  DRAW";
+                        label4.ForeColor = Color.Blue;
+                        break;
+                    case "Stein":
+                        label3.Text = "WINNER";
+                        label3.ForeColor = Color.Green;
+                        label4.Text = "  LOSER";
+                        label4.ForeColor = Color.Red;
+                        break;
+                    case "Papier":
+                        label3.Text = "  LOSER";
+                        label3.ForeColor = Color.Red;
+                        label4.Text = "WINNER";
+                        label4.ForeColor = Color.Green;
+                        break;
+                }
+                Task.Delay(2000).Wait();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+            }
+
+        }
+
+        private void BPaper_Click(object sender, EventArgs e)
+        {
+            NutzerEntscheidung = "Papier";
+            label2.Text = "📃";
+            AuswahlGegner();
+            BStone.Visible = false;
+            BScissour.Visible = false;
+            BPaper.Visible = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            labelVS.Visible = true;
+
+            if (MehrPunkte)
+            {
+                switch (SchereSteinPapier)
+                {
+                    case "Schere":
+                        label3.Text = "WINNER";
+                        label3.ForeColor = Color.Green;
+                        label4.Text = "  LOSER";
+                        label4.ForeColor = Color.Red;
+                        winBot++;
+                        break;
+                    case "Stein":
+                        label3.Text = "  LOSER";
+                        label3.ForeColor = Color.Red;
+                        label4.Text = "WINNER";
+                        label4.ForeColor = Color.Green;
+                        winPlayer++;
+                        break;
+                    case "Papier":
+                        label3.Text = "  DRAW";
+                        label3.ForeColor = Color.Blue;
+                        label4.Text = "  DRAW";
+                        label4.ForeColor = Color.Blue;
+                        break;
+                }
+                Task.Delay(2000).Wait();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+                Task.Delay(2000).Wait();
+                if (winBot + winPlayer == 3)
+                {
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    label1.Text = "PL.1";
+                    label2.Text = "PL.2";
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    Task.Delay(2000).Wait();
+                    if (winBot < winPlayer)
+                    {
+                        label3.Text = "WINNER";
+                        label4.Text = "  LOSER";
+                    }
+                    else if (winBot > winPlayer)
+                    {
+                        label3.Text = "  LOSER";
+                        label4.Text = "WINNER";
+                    }
+
+                    label1.Visible = false;
+                    label2.Visible = false;
+                    labelVS.Visible = false;
+                    Task.Delay(2000).Wait();
+                    label3.Visible = true;
+                    label4.Visible = true;
+                }
+                else
+                {
+                    SpielAnsicht();
+                }
+            }
+            else
+            {
+                switch (SchereSteinPapier)
+                {
+                    case "Schere":
+                        label3.Text = "WINNER";
+                        label3.ForeColor = Color.Green;
+                        label4.Text = "  LOSER";
+                        label4.ForeColor = Color.Red;
+                        break;
+                    case "Stein":
+                        label3.Text = "  LOSER";
+                        label3.ForeColor = Color.Red;
+                        label4.Text = "WINNER";
+                        label4.ForeColor = Color.Green;
+                        break;
+                    case "Papier":
+                        label3.Text = "  DRAW";
+                        label3.ForeColor = Color.Blue;
+                        label4.Text = "  DRAW";
+                        label4.ForeColor = Color.Blue;
+                        break;
+                }
+                Task.Delay(2000).Wait();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+            }
+
+
+        }
+
+        private void BStone_Click(object sender, EventArgs e)
+        {
+            NutzerEntscheidung = "Stein";
+            label2.Text = "🗿";
+            AuswahlGegner();
+            BStone.Visible = false;
+            BScissour.Visible = false;
+            BPaper.Visible = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            labelVS.Visible = true;
+
+            if (MehrPunkte)
+            {
+
+                switch (SchereSteinPapier)
+                {
+                    case "Schere":
+                        label4.Text = "WINNER";
+                        label4.ForeColor = Color.Green;
+                        label3.Text = "  LOSER";
+                        label3.ForeColor = Color.Red;
+                        winPlayer++;
+                        break;
+                    case "Stein":
+                        label3.Text = "  DRAW";
+                        label3.ForeColor = Color.Blue;
+                        label4.Text = "  DRAW";
+                        label4.ForeColor = Color.Blue;
+                        break;
+                    case "Papier":
+                        label3.Text = "WINNER";
+                        label3.ForeColor = Color.Green;
+                        label4.Text = "  LOSER";
+                        label4.ForeColor = Color.Red;
+                        winBot++;
+                        break;
+                }
+                Task.Delay(2000).Wait();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+                Task.Delay(2000).Wait();
+                if (winBot + winPlayer == 3)
+                {
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    label1.Text = "PL.1";
+                    label2.Text = "PL.2";
+                    label1.Visible = true;
+                    label2.Visible = true;
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    Task.Delay(2000).Wait();
+                    if (winBot < winPlayer)
+                    {
+                        label3.Text = "WINNER";
+                        label4.Text = "  LOSER";
+                    }
+                    else if (winBot > winPlayer)
+                    {
+                        label3.Text = "  LOSER";
+                        label4.Text = "WINNER";
+                    }
+
+                    label1.Visible = false;
+                    label2.Visible = false;
+                    labelVS.Visible = false;
+                    Task.Delay(2000).Wait();
+                    label3.Visible = true;
+                    label4.Visible = true;
+
+                }
+                else
+                {
+                    SpielAnsicht();
+                }
+            }
+            else
+            {
+
+                switch (SchereSteinPapier)
+                {
+                    case "Schere":
+                        label4.Text = "WINNER";
+                        label4.ForeColor = Color.Green;
+                        label3.Text = "  LOSER";
+                        label3.ForeColor = Color.Red;
+                        break;
+                    case "Stein":
+                        label3.Text = "  DRAW";
+                        label3.ForeColor = Color.Blue;
+                        label4.Text = "  DRAW";
+                        label4.ForeColor = Color.Blue;
+                        break;
+                    case "Papier":
+                        label3.Text = "WINNER";
+                        label3.ForeColor = Color.Green;
+                        label4.Text = "  LOSER";
+                        label4.ForeColor = Color.Red;
+                        break;
+                }
+                Task.Delay(2000).Wait();
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+            }
+
+        }
+
+        private void RESET_Click(object sender, EventArgs e)
+        {
+            SpielAnsicht();
+        }
+
+        private void B3pt_Click(object sender, EventArgs e)
+        {
+            SpielAnsicht();
+            MehrPunkte = true;
+
+        }
+
+        private void B1pt_Click(object sender, EventArgs e)
+        {
+            SpielAnsicht();
+            MehrPunkte = false;
+            RESET.Visible = true;
+
+        }
+
+        private void BMenu_Click(object sender, EventArgs e)
+        {
+            MenuAnsicht();
+        }
+    }
+}
